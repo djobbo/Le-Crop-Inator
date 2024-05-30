f4fe9f0fe2670864d204b8d7138c6b957ab1a00c
Djobbo <hi@djobbo.com>
2024-05-31 00:37:32 +0200

    split manga pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,131 @@
-import Image from "next/image";
+'use client'
+
+import { ChangeEventHandler, FormEvent, useRef, useState } from 'react';
+
+const getImageData = (image: Blob) => {
+  const fr = new FileReader();
+
+  return new Promise<HTMLImageElement>((resolve, reject) => {
+    fr.onload = () => {
+      if (!fr.result) {
+        reject(new Error("Failed to load image"));
+        return;
+      }
+
+      const img = document.createElement("img");
+      img.onload = () => {
+        resolve(img);
+      };
+      img.src = fr.result.toString();
+    };
+    fr.readAsDataURL(image);
+  });
+}
+
+const splitPageInTwo = (page: HTMLImageElement) => {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) return;
+
+  canvas.width = page.width / 2;
+  canvas.height = page.height;
+
+  ctx.drawImage(page, 0, 0, page.width / 2, page.height, 0, 0, page.width / 2, page.height);
+  const page1 = canvas.toDataURL();
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(page, page.width / 2, 0, page.width / 2, page.height, 0, 0, page.width / 2, page.height);
+  const page2 = canvas.toDataURL();
+
+  return [page1, page2];
+  
+}
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [files, setFiles] = useState<{file: File, split:boolean}[]>([]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const downloadPages = async (pages: {name: string, page:string}[]) => {
+    const a = document.createElement("a");
+    pages.forEach((page, i) => {
+      a.href = page.page;
+      a.download = page.name;
+      a.click();
+    })
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const splitPages = async () => {
+    if (!files) return;
+
+    const pagesPromises = Array.from(files).map(async ({file, split}) => {
+      if (!split) return {name: file.name, page: URL.createObjectURL(file)};
+
+      const image = await getImageData(file as Blob);
+      const [name, ext]= file.name.split(/(?=\.[^.]+$)/);
+      return splitPageInTwo(image)?.map((page, i) => ({name:`${name}-${i+1}${ext}`, page}));
+    })
+
+    const pages = (await Promise.all(pagesPromises)).flat().filter((page): page is {name: string, page:string} => !!page);
+
+    downloadPages(pages);
+  }
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    e.preventDefault();
+    const files =  e.target.files;
+
+    if (!files) return;
+
+    setFiles(Array.from(files).map(file => ({file, split: true})));
+  }
+
+  const toggleShouldSplit = (name: string) => {
+    setFiles(files.map(({file, split}) => ({file, split: file.name === name ? !split : split})));
+  }
+
+  return (
+    <>
+      <input type="file" name="images" multiple onChange={handleChange} ref={inputRef} className='hidden' />
+      <div className='flex flex-col items-center mt-20 gap-2'>
+        <h1 className='text-5xl'>Le Crop-Inator</h1>
+        <h2 className='text-lg'>Split manga pages</h2>
+      </div>
+      <div className='flex gap-4 mt-8 mx-auto w-full max-w-96'>
+      <a
+        className='block w-1/2 mx-auto py-2 px-4 bg-blue-500 text-white rounded-lg text-center cursor-pointer'
+        onClick={(e) => {
+          e.preventDefault();
+
+          if (inputRef.current) inputRef.current.click();
+        }}
+      >
+        Upload Files
+      </a>
+      <button
+        className='block w-1/2 mx-auto py-2 px-4 bg-blue-500 text-white rounded-lg text-center cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-500'
+        onClick={(e) => {
+          e.preventDefault();
+
+          splitPages()
+        }}
+        disabled={files.length === 0}
+        type='button'
+      >
+        Split Pages
+      </button>
+      </div>
+      {files.length > 0 && (
+        <>
+          <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 p-4'>
+            {files.map(({file, split}) => (
+              <button key={file.name} onClick={() => toggleShouldSplit(file.name)} className={`rounded-lg overflow-hidden border-4 ${split ? 'border-blue-500' : 'border-transparent'}`}>
+                <img src={URL.createObjectURL(file)} alt={file.name} />
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </>
   );
 }
